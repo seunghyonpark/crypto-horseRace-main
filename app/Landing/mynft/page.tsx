7a82ecaaeaa66ba44bc97632f814db3189f367d0
wayne309 <wayne@nuklabs.com>
2023-04-14 09:25:24 +0900

    change mypage

--- app/Landing/mynft/page.tsx
+++ app/Landing/mynft/page.tsx
@@ -796,23 +796,20 @@ export default function Mynft() {
 
 
 
+                <div className=" flex flex-col items-center justify-center md:w-1/3 bg-white rounded-lg shadow-md p-4 m-5">
 
-
-
-                <div className="flex flex-col justify-center h-full md:w-1/3 bg-white rounded-lg shadow-md p-4 m-5">
-
-<div className="pb-10 space-y-3">
-    <div className="flex gap-2 items-center pl-4">
-        <AiOutlineUser className="fill-green-500 w-5 h-5" />
-        <h2 className="text-gray-500 text-lg">
-            Connect Wallet
-        </h2>
-    </div>
-    <div className="w-full relative h-[1px] border flex items-center justify-center">
-        <div className="absolute bg-green-500 left-0 w-1/3 h-[1px] z-40"></div>
-        <div className="absolute left-1/3  w-2 h-2 rounded-full bg-green-500 z-50"></div>
-    </div>
-</div>
+                  <div className="pb-10 space-y-3">
+                      <div className="flex gap-2 items-center pl-4">
+                          <AiOutlineUser className="fill-green-500 w-5 h-5" />
+                          <h2 className="text-gray-500 text-lg">
+                              Connect Wallet
+                          </h2>
+                      </div>
+                      <div className="w-full relative h-[1px] border flex items-center justify-center">
+                          <div className="absolute bg-green-500 left-0 w-1/3 h-[1px] z-40"></div>
+                          <div className="absolute left-1/3  w-2 h-2 rounded-full bg-green-500 z-50"></div>
+                      </div>
+                  </div>
 
 {/* //todo BU KISIMA METAMASK EKLENİCEK */}
 

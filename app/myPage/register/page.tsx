24bc22f7d8105063bb1b4c3387c010d545ba44aa
wayne309 <wayne@nuklabs.com>
2023-04-30 17:14:04 +0900

    update more and more

--- app/myPage/register/page.tsx
+++ app/myPage/register/page.tsx
@@ -512,10 +512,13 @@ export default function RegisterPage() {
 
     <div className="pb-0 space-y-3">
         <div className="flex gap-2 items-center pl-4">
+            {/*
             <VscGear className="fill-red-500 w-5 h-5" />
             <h2 className="text-gray-500 text-lg">
                 Personal Information
             </h2>
+    */}
+            <h2 className="text-4xl  text-gray-500">Sign Up</h2>
         </div>
         <div className="w-full relative h-[1px] border flex items-center justify-center">
             <div className="absolute bg-red-500 left-0 w-1/3 h-[1px] z-40"></div>
